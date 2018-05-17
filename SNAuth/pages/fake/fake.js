990aceb76c19cb87ba29516cfb19b662a39c3876
vacuole1989 <154034117@qq.com>
2018-05-17 18:27:35 +0800

    1

--- SNAuth/pages/fake/fake.js
+++ SNAuth/pages/fake/fake.js
@@ -59,147 +59,134 @@ Page({
                     if (mm.indexOf('<') > -1) {
                         ress.model = mm.substring(0, mm.indexOf('<'));
                     }
-                    wx.login({
-                        success: resl => {
-                            app.globalData.code = resl.code;
-                            wx.getShareInfo({
-                                shareTicket: app.globalData.ticket,
-                                success(res) {
-                                    wx.request({
-                                        method: 'POST',
-                                        url: config.service.GetGroupId,
-                                        data: {
-                                            code: resl.code,
-                                            encryptedData: res.encryptedData,
-                                            iv: res.iv,
-                                            avatarUrl: e.detail.userInfo.avatarUrl,
-                                            phone: ress.model,
-                                            nickName: e.detail.userInfo.nickName
-                                        },
-                                        header: {
-                                            'content-type': 'application/json' // 默认值
-                                        },
-                                        success: function (res) {
-                                            if (res.data.success) {
-                                                _this.setData({
-                                                    show: true,
-                                                    showList: true,
-                                                    compList: res.data.data
-                                                })
-                                            } else {
-                                                _this.setData({
-                                                    show: true,
-                                                    showList: false
-                                                })
-                                            }
-                                        }
-                                    })
-                                },
-                                fail() { },
-                                complete() { }
-                            });
-                        }
-                    })
-
+                    _this.initLogin(ress.model,true,e.detail.userInfo.avatarUrl,e.detail.userInfo.nickName)
                 }
             })
         }
+    },
+    GetGroupIdFnc:function(code,encryptedData,iv,model,avatarUrl,nickName){
+        var _this = this;
+        wx.request({
+            method: 'POST',
+            url: config.service.getGroupId,
+            data: {
+                code: code,
+                encryptedData: encryptedData,
+                iv: iv,
+                phone: model,
+                avatarUrl: avatarUrl,
+                nickName: nickName
+            },
+            success: function (res) {
+                if (res.data.success) {
+                    _this.setData({
+                        show: true,
+                        showList: true,
+                        compList: res.data.data
+                    })
+                } else {
+                    _this.setData({
+                        show: true,
+                        showList: false
+                    })
+                }
+            }
+        });
+    },
+    ifUserAuth:function(code,encryptedData,iv,model){
+        var _this = this;
+        wx.getSetting({
+            success(resa) {
+                if (resa.authSetting['scope.userInfo']) {
+                    _this.setData({
+                        userAuth: true
+                    });
+                    wx.getUserInfo({
+                        success(resu) {
+                            _this.GetGroupIdFnc(code,encryptedData,iv,model,resu.userInfo.avatarUrl,resu.userInfo.nickName);
+                        }
+                    });
+                } else {
+                    _this.setData({
+                        userAuth: false,
+                        show: true,
+                        showList: false
+                    });
+                }
+            }
+        })
+    },
+    ifUserAuthBefore:function(code,encryptedData,iv,model){
+        var _this = this;
+        wx.request({
+            method: 'POST',
+            url: config.service.hasGroupId,
+            data: {
+                code: code,
+                encryptedData: encryptedData,
+                iv: iv,
+                phone: model
+            },
+            success: function (res) {
+                if (res.data.success) {
+                    _this.setData({
+                        show: true,
+                        showList: true,
+                        compList: res.data.data
+                    })
+                } else {
+                    _this.ifUserAuth(code,encryptedData,iv,model);
+                }
+            }
+        });
+    },
+    initLogin:function(model,isPressBtn,avatarUrl,nickName){
+        var _this = this;
+        wx.login({
+            success(resl) {
+                app.globalData.code = resl.code;
+                wx.getShareInfo({
+                    shareTicket: app.globalData.ticket,
+                    success(resi) {
+                        if(isPressBtn){
+                            _this.GetGroupIdFnc(resl.code,resi.encryptedData,resi.iv,model,avatarUrl,nickName);
+                        }else{
+                            _this.ifUserAuthBefore(resl.code,resi.encryptedData,resi.iv,model);
+                        }
+                    }
+                })
+            }
+        })
     },
     onLoad: function () {
         var _this = this;
         wx.showShareMenu({
             withShareTicket: true
         });
-        if (null != app.globalData.ticket) {
-            wx.getSystemInfo({
-                success: function (ress) {
-
-                    var mm = ress.model;
-                    if (mm.indexOf('<') > -1) {
-                        ress.model = mm.substring(0, mm.indexOf('<'));
-                    }
-                    ress.screenHeight = ress.screenHeight * ress.pixelRatio;
-                    ress.screenWidth = ress.screenWidth * ress.pixelRatio;
-                    wx.getUserInfo({
-                        success: function (resu) {
-                            wx.login({
-                                success: resl => {
-                                    app.globalData.code = resl.code;
-                                    wx.getShareInfo({
-                                        shareTicket: app.globalData.ticket,
-                                        success(res) {
-                                            wx.request({
-                                                method: 'POST',
-                                                url: config.service.GetGroupId,
-                                                data: {
-                                                    code: resl.code,
-                                                    encryptedData: res.encryptedData,
-                                                    iv: res.iv,
-                                                    avatarUrl: resu.userInfo.avatarUrl,
-                                                    phone: ress.model,
-                                                    nickName: resu.userInfo.nickName
-                                                },
-                                                header: {
-                                                    'content-type': 'application/json' // 默认值
-                                                },
-                                                success: function (res) {
-                                                    if (res.data.success) {
-                                                        _this.setData({
-                                                            show: true,
-                                                            showList: true,
-                                                            compList: res.data.data
-                                                        })
-                                                    } else {
-                                                        _this.setData({
-                                                            show: true,
-                                                            showList: false
-                                                        })
-                                                    }
-                                                }
-                                            })
-                                        },
-                                        fail() { },
-                                        complete() { }
-                                    });
-                                }
-                            })
-                        }
-                    });
-                }
-            })
-        } else {
-            _this.setData({
-                show: true
-            })
-        }
         _this.setData({
             showAuth: app.globalData.showAuth
-        })
-        wx.getSetting({
-            success(res) {
-                if (res.authSetting['scope.userInfo']) {
-                    _this.setData({
-                        userAuth: true
-                    })
+        });
+
+        wx.getSystemInfo({
+            success(ress) {
+                var mm = ress.model;
+                if (mm.indexOf('<') > -1) {
+                    ress.model = mm.substring(0, mm.indexOf('<'));
+                }
+                ress.screenHeight = ress.screenHeight * ress.pixelRatio;
+                ress.screenWidth = ress.screenWidth * ress.pixelRatio;
+                ress.language = locale[ress.language];
+                _this.setData({
+                    sysData: ress
+                });
+
+                if (null != app.globalData.ticket) {
+                    _this.initLogin(ress.model,false,null,null);
                 } else {
                     _this.setData({
-                        userAuth: false
+                        show: true
                     })
                 }
-            }
-        })
-        wx.getSystemInfo({
-            success: function (res) {
-                var mm = res.model;
-                if (mm.indexOf('<') > -1) {
-                    res.model = mm.substring(0, mm.indexOf('<'));
-                }
-                res.screenHeight = res.screenHeight * res.pixelRatio;
-                res.screenWidth = res.screenWidth * res.pixelRatio;
-                res.language = locale[res.language];
-                _this.setData({
-                    sysData: res
-                })
             }
         })
     }
